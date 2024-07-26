c6f77cca7e84e9395f3f72e1368a406e4ad9cab0
HCPTangHY <tanghy06025@163.com>
2024-07-26 14:04:28 +0800

    fix replacer

--- src/replacer.py
+++ src/replacer.py
@@ -1,5 +1,5 @@
 import ujson as json
-import os
+import os,re
 import shutil
 from .consts import *
 from .log import logger
@@ -52,14 +52,15 @@ class Replacer:
                     flines = fp.readlines()
                 trans_file_path = file_path.replace('source', 'trans', 1).replace(".gd", ".json")
 
-                logger.info(f"trans_file {trans_file_path} in {file_path}"
+                # logger.info(f"trans_file {trans_file_path} in {file_path}")
                 if trans_file_path not in self.translationDict:
                     shutil.copyfile(file_path, output_path)
                     # logger.info(f"trans_file {trans_file_path}")
                     continue
-                logger.info(f"trans_file {trans_file_path}")
+                # logger.info(f"trans_file {trans_file_path}")
                 file_hash_index = self.hashIndex[file_path.replace(self.BDCCSourcePath + "\\", "", 1)]
                 trans_dict = self.translationDict[trans_file_path]
+                lineWrited = []
                 for i in range(len(trans_dict)):
                     trans_value = trans_dict[i]
                     index = int(trans_value['key'].split("_")[-1])
@@ -67,19 +68,63 @@ class Replacer:
                     tokenPostion = file_hash_index["TokenPostion"][hash][0]
                     translation = trans_value['translation'] if trans_value['translation'] != "" else trans_value[
                         'original']
-                    translationParse = translation.split("\n")
-                    if len(translationParse)>1:
-                        strFlag = False
-                        for i in range(len(translationParse)):
-                            if i+1==len(translation):break
-                            if translationParse[i]==translationParse[i+1]=="\"":
-                                strFlag = True
-                                break
-                        if strFlag:translation = translation.replace("\n", "\\n")
-                        else:translation=translation.replace("\n","")
+                    # translationParse = translation.split("\n")
+                    # if len(translationParse)>1:
+                    #     strFlag = False
+                    #     for i in range(len(translationParse)):
+                    #         if i+1==len(translation):break
+                    #         if translationParse[i]==translationParse[i+1]=="\"":
+                    #             strFlag = True
+                    #             break
+                    #     if strFlag:translation = translation.replace("\n", "\\n")
+                    #     else:translation=translation.replace("\n","")
                     context = trans_value['context'] if 'context' in trans_value else trans_value['original']
                     targetLine = []
                     lineIndex = tokenPostion["StartLine"]
+                    endLineIndex = tokenPostion["EndLine"]
+                    if '"""' in translation:
+                        translationLines = translation.replace("\\n","\n").split("\n")
+                        translationLines[0] = flines[lineIndex][:tokenPostion["StartColumn"]]+translationLines[0]
+                        translationLines[-1] += flines[endLineIndex][tokenPostion["EndColumn"]:]
+                        nowLine = lineIndex
+                        for l in translationLines:
+                            try:
+                                flines[nowLine] = l+"\n"
+                                nowLine+=1
+                            except:
+                                logger.error(f"{f},{index}")
+                        lineWrited.append(lineIndex)
+                        continue
+                    if lineIndex!=endLineIndex:
+                        translation = translation.replace("\\n","\n")
+                        if '"""' not in translation:
+                            tokens = re.split(r"(\")",translation)
+                            strFlag = False
+                            for i in range(len(tokens)):
+                                if tokens[i]=="\"":
+                                    strFlag=not strFlag
+                                    continue
+                                if strFlag:
+                                    tokens[i] = tokens[i].replace("\n","\\n")
+                            translation = "".join(tokens)
+                        else:
+                            print(translation)
+                        translationLines = translation.split("\n")
+                        try:
+                            translationLines[0] = flines[lineIndex][:tokenPostion["StartColumn"]]+translationLines[0]
+                            translationLines[-1] += flines[endLineIndex][tokenPostion["EndColumn"]:]
+                        except:
+                            logger.error(f"{f},{index}")
+                            continue
+                        nowLine = lineIndex
+                        for l in translationLines:
+                            try:
+                                flines[nowLine] = l+"\n"
+                                nowLine+=1
+                            except:
+                                logger.error(f"{f},{index}")
+                        lineWrited.append(lineIndex)
+                        continue
                     if lineIndex >= len(flines): lineIndex -= 1
                     if context not in flines[lineIndex]:
                         if context in flines[min(lineIndex + 1, len(flines) - 1)]:
@@ -87,12 +132,16 @@ class Replacer:
                         else:
                             if context in flines[lineIndex - 1]: lineIndex -= 1
                     try:
-                        targetLine.append(flines[lineIndex][:tokenPostion["StartColumn"]])
-                        targetLine.append(translation)
-                        targetLine.append(flines[lineIndex][tokenPostion["EndColumn"]:])
+                        if lineIndex in lineWrited:
+                            targetLine = flines[lineIndex].replace(f"\"{trans_value['original']}\"",f"\"{translation}\"")
+                        else:
+                            targetLine.append(flines[lineIndex][:tokenPostion["StartColumn"]])
+                            targetLine.append(translation)
+                            targetLine.append(flines[lineIndex][tokenPostion["EndColumn"]:])
                         flines[lineIndex] = "".join(targetLine)
+                        lineWrited.append(lineIndex)
                     except:
                         logger.error(f"{f},{index}")
                 with open(output_path, "w", encoding="utf-8") as fp:
                     fp.write("".join(flines))
-                logger.info(f"{output_path} output filled")
+                # logger.info(f"{output_path} output filled")
