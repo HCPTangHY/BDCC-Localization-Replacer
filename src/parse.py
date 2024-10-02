bd042c1300cf8797d47e0c497eaf25cbca9bfea6
CK Rainbow <nanashichi@proton.me>
2024-10-02 22:48:57 +0800

    Checking: fix

--- src/parse.py
+++ src/parse.py
@@ -8,18 +8,18 @@ def parser_check(output_path: Union[Path, str]):
     if isinstance(output_path, str):
         output_path = Path(output_path)
 
-        is_error = False
-        for file in output_path.glob("**/*.gd"):
-            with open(file, "r", encoding="utf-8") as f:
-                code = f.read()
+    is_error = False
+    for file in output_path.glob("**/*.gd"):
+        with open(file, "r", encoding="utf-8") as f:
+            code = f.read()
 
-            try:
-                parser.parse(code)
-            except Exception as e:
-                is_error = True
-                logger.error(f"{file} parse error: {e}")
+        try:
+            parser.parse(code)
+        except Exception as e:
+            is_error = True
+            logger.error(f"{file} parse error: {e}")
 
-        if is_error:
-            raise Exception("parse error")
-        else:
-            logger.info("parse success")
+    if is_error:
+        raise Exception("parse error")
+    else:
+        logger.info("parse success")
