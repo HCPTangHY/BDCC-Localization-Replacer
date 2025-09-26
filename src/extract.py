2e283b3c1e0f8adcc361d20acb86a8e7171074c7
CK Rainbow <nanashichi@proton.me>
2025-09-26 22:57:55 +0800

    update

--- src/extract.py
+++ src/extract.py
@@ -205,7 +205,8 @@ def extract(source_path: Union[Path, str], result_path: Union[Path, str]):
                 or "Description = " in line
                 or "tooltip = " in line
                 or "title = " in line
-                or "Message = " in line
+                or "Message= " in line
+                or "Desc= " in line
             ) and '"' in line:
                 start_line = idx
             if new_node:
